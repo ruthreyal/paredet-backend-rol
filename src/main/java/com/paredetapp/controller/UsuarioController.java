02e2c2553c17e9e6143bd824ed0454a00231a8d2
ruthreyal <r.reyes.alvarez@gmail.com>
2025-04-27 13:11:38 +0200

    fix: añadir import de PreAuthorize en UsuarioController para corregir error de compilación

--- src/main/java/com/paredetapp/controller/UsuarioController.java
+++ src/main/java/com/paredetapp/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import com.paredetapp.model.Usuario;
 import com.paredetapp.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
