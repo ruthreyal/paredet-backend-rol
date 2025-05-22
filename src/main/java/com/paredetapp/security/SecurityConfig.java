e4fcfdc16762c8cb248ae172b3d89a4b139dcebe
ruthreyal <r.reyes.alvarez@gmail.com>
2025-05-22 18:31:32 +0200

    modificacion usuario alineacion con bd

--- src/main/java/com/paredetapp/security/SecurityConfig.java
+++ src/main/java/com/paredetapp/security/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/usuarios/email-existe").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/productos/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/categorias/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/colecciones/**").permitAll()
