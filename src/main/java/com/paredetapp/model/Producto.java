e75c41d03623a7590c40cb3d86a0234e7510a0ad
ruthreyal <r.reyes.alvarez@gmail.com>
2025-05-25 18:02:13 +0200

    validaciones en productos

--- src/main/java/com/paredetapp/model/Producto.java
+++ src/main/java/com/paredetapp/model/Producto.java
@@ -2,28 +2,52 @@ package com.paredetapp.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+
 import java.util.UUID;
 
 @Data
 @Entity
-@Table(name = "productos")
+@Table(
+        name = "productos",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"nombre"}) // nombre único
+        }
+)
 public class Producto {
 
     @Id
     @GeneratedValue
     private UUID id;
 
+    @Column(nullable = false, length = 255)
     private String nombre;
+
+    @Column(columnDefinition = "TEXT")
     private String descripcion;
+
+    @Column(nullable = false)
     private double precio;
+
     private double precioRecomendado;
+
+    @Column(nullable = false)
     private int stock;
 
+    @Column(length = 255)
     private String imagenUrl;
+
+    @Column(length = 100)
     private String referencia;
+
+    @Column(length = 100)
     private String formato;
+
+    @Column(length = 100)
     private String tipoAplicacion;
+
     private double peso;
+
+    @Column(length = 100)
     private String familia;
 
     @ManyToOne
@@ -34,3 +58,4 @@ public class Producto {
     @JoinColumn(name = "coleccion_id")
     private Coleccion coleccion;
 }
+
