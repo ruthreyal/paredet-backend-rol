5ed339fb83325e0de8453450d29931c88d7fa607
ruthreyal <r.reyes.alvarez@gmail.com>
2025-06-01 12:42:41 +0200

    añadir metodo y endpoint para calcular total mensual

--- src/main/java/com/paredetapp/repository/PedidoRepository.java
+++ src/main/java/com/paredetapp/repository/PedidoRepository.java
@@ -11,8 +11,9 @@ public interface PedidoRepository extends JpaRepository<Pedido, UUID> {
 
     List<Pedido> findByUsuario_Id(UUID idUsuario);
 
-    @Query("SELECT SUM(p.total) FROM Pedido p WHERE MONTH(p.fechaCreacion) = MONTH(CURRENT_DATE) AND YEAR(p.fechaCreacion) = YEAR(CURRENT_DATE)")
+    @Query("SELECT SUM(p.total) FROM Pedido p WHERE EXTRACT(MONTH FROM p.fechaCreacion) = EXTRACT(MONTH FROM CURRENT_DATE) AND EXTRACT(YEAR FROM p.fechaCreacion) = EXTRACT(YEAR FROM CURRENT_DATE)")
     Double obtenerTotalVentasMesActual();
+
 
 
 }
